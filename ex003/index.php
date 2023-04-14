bb085abe7de051da925a2d62063d313da5f3ca82
capeloo <caiocapelo@alu.ufc.br>
2023-04-14 19:16:03 -0300

    tipos primitivos

--- ex003/index.php
+++ ex003/index.php
@@ -9,18 +9,38 @@
 <body>
     <h1>Teste de tipos primitivos</h1>
     <?php 
+        //tipo primitivos escalares:
+            //int
+            //string
+            //float
+            //boolean
+        //tipos primitivos compostos:
+            //array
+            //object    
+        //tipos primitivos especiais:
+            //null
+            //resource
+            //callabe
+            //mixed
+
         //0x = hexadecimal 0b = binário 0 = Octal
+
         $num = 0x1A;
         //
         $v = 45.2;
         //
         $n = 3e2; //3x(10)^2
         //
+        $x = (int) "120"; //coerção
+        //
+        $vet = [6, 2.5, "Maria", false];
+        //
+        class Pessoa {
+            private string $nome;
+        }
+        $p = new Pessoa;
+
         echo "O valor da variável num é $num, o da n é $n e o tipo da variável v é " . var_dump($v);
-        
-
-
-
     ?>
 </body>
 </html>
