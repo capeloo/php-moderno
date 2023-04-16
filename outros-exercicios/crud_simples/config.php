f670663fffd1268e593d0c6721d6fe1a4483a91b
capeloo <caiocapelo@alu.ufc.br>
2023-04-16 15:06:46 -0300

    bootstrap + config.php

---
+++ outros-exercicios/crud_simples/config.php
@@ -0,0 +1,8 @@
+<?php 
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASSWORD', '');
+    define('DB', 'meu_banco');
+
+    $connection = new mysqli(HOST, USER, PASSWORD, DB);
+?>
